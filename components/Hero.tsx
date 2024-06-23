2816cfa6db741ad39916fda9c2508576a1b9f719
Revanth Reddy <revanthreddy4666@gmail.com>
2024-06-23 19:08:48 +0530

    init

---
+++ components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Spotlight } from './ui/Spotlight'
+import { LampDemo } from './ui/Lamp'
+
+const Hero = () => {
+  return (
+    <div className='pb-20 pt-36'>
+      <LampDemo></LampDemo>
+
+    </div>
+  )
+}
+
+export default Hero
