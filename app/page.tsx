ab4f3d9d3905db97a29829e21f643d364d26d800
Revanth Reddy <revanthreddy4666@gmail.com>
2024-06-26 13:00:18 +0530

    slideshow

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import Hero from "@/components/Hero";
-import { FloatingNav } from "@/components/ui/Navbar";
+import Navbar from "@/components/ui/Navbar";
 import { navItems } from "@/data";
 import Image from "next/image";
 
 export default function Home() {
-  return (
+  return ( //tailwind works, so does its extension
     <main>
-      <div>
-        <FloatingNav navItems={navItems}/>
+      <div className="min-h-screen flex flex-col">
+        <Navbar></Navbar>
         <Hero/>
       </div>
     </main>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,14 +1,48 @@
-import React from 'react'
+"use client";
+import { cn } from "@/utils/cn";
+import { motion } from "framer-motion";
+import React from "react";
 import { LampDemo } from './ui/Lamp'
-import { motion } from 'framer-motion'
+import { ImagesSlider } from "./ui/Slider";
 
 const Hero = () => {
-
   return(
-    <LampDemo></LampDemo>
+    <ImagesSliderDemo />
   );
-
 }
 
-
 export default Hero
+
+export function ImagesSliderDemo() {
+  const images = [
+    "https://images.unsplash.com/photo-1485433592409-9018e83a1f0d?q=80&w=1814&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1483982258113-b72862e6cff6?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1482189349482-3defd547e0e9?q=80&w=2848&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  ];
+  return (
+    <ImagesSlider className="h-screen" images={images}>
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: -80,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 0.6,
+        }}
+        className="z-50 flex flex-col justify-center items-center"
+      >
+        <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-300 to-neutral-600 py-4">
+          Step into the future <br /> with <span className="text-[#6b34d8]">GTXR</span> 
+        </motion.p>
+        <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
+          <span>Join Discord Now →</span>
+          <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
+        </button>
+      </motion.div>
+    </ImagesSlider>
+  );
+}
