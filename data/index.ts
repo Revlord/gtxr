7a6bdf7ff94ca407f1210a8904a7b458685fb53e
Revanth Reddy <revanthreddy4666@gmail.com>
2024-07-14 22:18:56 +0530

    navbar items

--- data/index.ts
+++ data/index.ts
@@ -1,6 +1,14 @@
+/**
+ * This file includes all the variables that represent content on the website.
+ * Exec members can fill in/edit the texts || descriptions if required.
+ */
+
+
+//variable for items displayed in the navbar
 export const navItems = [
     { name: "About", link: "#about" },
     { name: "Projects", link: "#projects" },
-    { name: "Events", link: "#Work" },
-    { name: "Hackathon", link: "#contact" },
+    { name: "Events", link: "#events" },
+    { name: "Hackathon", link: "#hackathon" },
+    { name: "Sponsorship", link: "#sponsorship" },
   ];
