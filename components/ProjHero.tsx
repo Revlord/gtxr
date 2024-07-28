4132848e9cd9347a4c7c1e21fdaba3dcbcd77b84
Revanth Reddy <revanthreddy4666@gmail.com>
2024-07-28 14:48:58 +0530

    changed to new footer + about page changes + added components for projects page setup

--- components/ProjHero.tsx
+++ components/ProjHero.tsx
@@ -12,9 +12,7 @@ const ProjHero = () => {
     <div className='relative w-full min-h-[calc(100vh)] h-fit flex bg-black mt-4'>
       <div className="h-auto bg-neutral-900 flex flex-col flex-grow m-8 items-center justify-center relative w-full rounded-[3rem]">
       <h2 className="relative flex-col md:flex-row z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8">
-        <span>Shooting Star</span>
-        <span className="text-white text-lg font-thin">x</span>
-        <span>Star Background</span>
+        Our Projects
       </h2>
       <ShootingStars />
       <StarsBackground />
