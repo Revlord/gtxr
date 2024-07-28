61bd952364c85a129f6d64417c4dfa83a0e74c94
Revanth Reddy <revanthreddy4666@gmail.com>
2024-07-29 01:19:24 +0530

    discord link fix

--- components/ThreeDCard.tsx
+++ components/ThreeDCard.tsx
@@ -44,7 +44,7 @@ export function ThreeDCardDemo() {
           <CardItem
             translateZ={20}
             as={Link}
-            href="https://discord.gg/rVRTd4Q4"
+            href="https://discord.gg/GGBEuSHZHX"
             className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
           >
             Discord
