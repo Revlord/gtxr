3fdca098d5062bbdc870a88d2b17bcf87d8baf06
Revanth Reddy <revanthreddy4666@gmail.com>
2025-09-19 18:50:05 -0400

    deploy fix?

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,15 +1,11 @@
-"use client"
-
+'use client'
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
-import { WebSpatialProvider } from "@webspatial/react-sdk";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
-    <WebSpatialProvider>
-      <NextThemesProvider {...props}>{children}</NextThemesProvider>
-    </WebSpatialProvider>
+    <NextThemesProvider {...props}>{children}</NextThemesProvider>
   );
 }
